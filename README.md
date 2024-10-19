8604d690e94c841ce34905baa80cfc3f5be71d9a
Nikolaos987 <73761101+Nikolaos987@users.noreply.github.com>
2024-10-19 23:33:20 +0300

    database creation as a JSON file with 2 blog post objects
    home page that shows all of the blog posts

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BlogVerse
 Write, Share, Inspire - Build Your Blogging Haven
+
+How to get started:
+- install all dependencies stated in package.json file for the project typing the command "npm install" inside the backend folder from the terminal
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("Welcome to BlogVerse <br>Start Building Your Blogging Haven!");
+    fs.readFile("public/posts/data.json", (err, data) => {
+        if (err) {
+            return res.status(500).send("Error reading data from JSON file");
+        }
+
+        const posts = JSON.parse(data);
+
+        // for each post read the txt file that contains the blog text from the textPath property
+        const postsWithContent = posts.map((post) => {
+            try {
+                const fullTextPath = post.textPath;
+                // console.log("Trying to read file from: ", fullTextPath);
+                const textContent = fs.readFileSync(post.textPath, 'utf-8');
+                return { ...post, content: textContent };
+            } catch (error) {
+                return { ...post, content: "Error trying to load the content"};
+            }
+        });
+
+        res.render("index.ejs", { posts: postsWithContent })
+    });
+
 });
 
 app.listen(port , () => {
